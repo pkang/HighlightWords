18fbc59d841281d2a9670c7f584943ff3e5c324d
Pablo Kang <pablokang@gmail.com>
2010-10-12 01:18:13 -0700

    Just the bare essentials.

--- Classes/HighlightWordsViewController.m
+++ Classes/HighlightWordsViewController.m
@@ -9,42 +9,14 @@
 #import "HighlightWordsViewController.h"
 #import "TextView.h"
 
-static NSString * const paragraph = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Nam mi nisi, vestibulum eu ullamcorper vel, ultrices eu enim. \
-Aenean molestie, massa a venenatis tempus, lorem massa mattis lorem, \
-sit amet hendrerit urna erat et augue. Pellentesque elementum, \
-lectus a tincidunt blandit, justo mi aliquet erat, nec luctus justo dolor id ligula. \
-Vestibulum auctor felis quis dolor dapibus sed tincidunt sem fermentum.";
-
 @implementation HighlightWordsViewController
 
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
 	TextView *textView = [[TextView alloc] initWithFrame:CGRectMake(20, 20, 600, 600)];
 	textView.backgroundColor = [UIColor yellowColor];
 	[self.view addSubview:textView];
 }
-
-
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    return YES;
-}
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
 
 - (void)dealloc {
     [super dealloc];
--- Classes/TextView.m
+++ Classes/TextView.m
@@ -14,52 +14,7 @@
 @implementation TextView
 
 
-- (id)initWithFrame:(CGRect)frame {
-	NSLog(@"init with frame");
-    if ((self = [super initWithFrame:frame])) {
-        // Initialization code
-    }
-    return self;
-}
-
-
 - (void)drawRect:(CGRect)rect {
-//	// Initialize a graphics context and set the text matrix to a known value.
-//	CGContextRef context = UIGraphicsGetCurrentContext();
-//	CGContextSetTextMatrix(context, CGAffineTransformIdentity);
-////	CGContextSetTextMatrix(context, CGAffineTransformMakeScale(1, -1));
-//	
-//	// Initialize a rectangular path.
-//	CGMutablePathRef path = CGPathCreateMutable();
-//	CGRect bounds = CGRectMake(10.0, 10.0, 200.0, 300.0);
-//	CGPathAddRect(path, NULL, bounds);
-//	
-//	// Initialize an attributed string.
-//	NSString *myString = @"We hold this truth to be self-evident, that everyone is created equal.";
-//	NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:myString];
-//	
-//	// Create a color and add it as an attribute to the string.
-//	CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-//	CGFloat components[] = { 1.0, 0.0, 0.0, 1.0 };
-//	CGColorRef red = CGColorCreate(rgbColorSpace, components);
-//	CGColorSpaceRelease(rgbColorSpace);
-//	CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attrString, CFRangeMake(13, 5), kCTForegroundColorAttributeName, red);
-//
-//	CTFontRef font = CTFontCreateWithName(CFSTR("Times"), 21, NULL);
-//	CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attrString, CFRangeMake(0, [attrString length]), kCTFontAttributeName, font);
-//
-//	// Create the framesetter with the attributed string.
-//	CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attrString);
-//	
-//	// Create the frame and draw it into the graphics context
-//	CTFrameRef myFrame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, [attrString length]), path, NULL);
-//	CTFrameDraw(myFrame, context);
-//
-//	[attrString release];
-//	CFRelease(framesetter);
-//	CFRelease(myFrame);
-//	CFRelease(path);
-	
 	// http://forums.macrumors.com/showthread.php?t=925312
 	CGContextRef context = UIGraphicsGetCurrentContext();
 	
@@ -98,10 +53,5 @@
 	
 	CFRelease(maString);
 }
-
-- (void)dealloc {
-    [super dealloc];
-}
-
 
 @end
